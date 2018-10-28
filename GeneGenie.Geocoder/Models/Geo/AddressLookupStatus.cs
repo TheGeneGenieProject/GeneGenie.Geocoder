07e422a1636571781069a7f2e176bf9dd14987b3
RyanONeill1970 <r@ryanoneill.com>
2018-10-28 23:24:29 +0000

    Cut down unused enum values in AddressLookupStatus and document remaining items. Bump NuGet.

--- GeneGenie.Geocoder/Models/Geo/AddressLookupStatus.cs
+++ GeneGenie.Geocoder/Models/Geo/AddressLookupStatus.cs
@@ -6,39 +6,24 @@
 namespace GeneGenie.Geocoder.Models.Geo
 {
     /// <summary>
-    /// The status of a address lookup and whether it is worth parsing it.
-    /// Values less than zero are not worth looking up and are known
-    /// bad values. Zero is uninitialised and greater than zero
-    /// means it is in the geocoding workflow.
+    /// The status of an address lookup.
     /// </summary>
     public enum AddressLookupStatus
     {
-        SeemsToBeADate = -103,
-
-        AllNumeric = -102,
-
-        KnownErroneous = -101,
-
-        SkippedAsEmpty = -100,
-
-        NotSet = 0,
-
-        /// <summary>
-        /// Has passed the basic quality tests and needs looking up.
-        /// </summary>
-        RequiresLookup = 100,
-
-        [System.Obsolete("Possibly needs deleting as we don't cache any more")]
-        RequiresGeocoding = 101,
-
         /// <summary>
         /// All available geocoders were queried and a mix of non success results were returned.
         /// If this happens a lot, we need to see why.
         /// </summary>
         MultipleIssues = 101,
 
+        /// <summary>
+        /// Geocoder or network error, the operation might work if you try it again (maybe keep a count of the number of tried to avoid an indefinite loop).
+        /// </summary>
         TemporaryGeocodeError = 102,
 
+        /// <summary>
+        /// Address could not be geocoded by any of the geocoding services. You should check the source data and ask for a user to intervene.
+        /// </summary>
         PermanentGeocodeError = 103,
 
         /// <summary>
@@ -47,6 +32,9 @@ namespace GeneGenie.Geocoder.Models.Geo
         /// </summary>
         ZeroResults = 104,
 
+        /// <summary>
+        /// Address was successfully geocoded.
+        /// </summary>
         Geocoded = 105,
     }
 }
