3695cac903490c2dd54f5c777a5e6d5eeb1f811b
RyanONeill1970 <r@ryanoneill.com>
2018-11-12 11:07:23 +0000

    Logging; Tidy up code header.

--- GeneGenie.Geocoder.Tests/Fakes/FakeLogger.cs
+++ GeneGenie.Geocoder.Tests/Fakes/FakeLogger.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Fakelogger.cs" company="GeneGenie.com">
+﻿// <copyright file="FakeLogger.cs" company="GeneGenie.com">
 // Copyright (c) GeneGenie.com. All Rights Reserved.
 // Licensed under the GNU Affero General Public License v3.0. See LICENSE in the project root for license information.
 // </copyright>
