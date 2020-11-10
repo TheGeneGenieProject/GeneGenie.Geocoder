8f3d82d557bdbbbe194fa6755692a1c59fc34feb
Ryan O'Neill <r@ryanoneill.com>
2020-11-10 11:34:18 +0000

    Fix SonarQube scan on deploy.

--- SonarQube.bat
+++ SonarQube.bat
@@ -1,4 +1,4 @@
-dotnet tool install --global dotnet-sonarscanner
+dotnet tool install --global dotnet-sonarscanner --version 4.10.0
 dotnet test GeneGenie.Geocoder.Tests\GeneGenie.Geocoder.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput="%CD%\opencover.xml"
 dotnet build-server shutdown
 dotnet sonarscanner begin /k:"GeneGenie.Geocoder" /o:"thegenegenieproject" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=%SonarQubeApiKey% /d:sonar.cs.opencover.reportsPaths="%CD%\opencover.xml"
