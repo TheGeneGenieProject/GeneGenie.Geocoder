2c09840654814a8024f9ef53b59b36b23f2d377d
Ryan O'Neill <r@ryanoneill.com>
2022-12-06 14:19:04 +0000

    More GEO docs.

--- GeneGenie.Geocoder/Dto/Bing/Point.cs
+++ GeneGenie.Geocoder/Dto/Bing/Point.cs
@@ -5,14 +5,37 @@
 
 namespace GeneGenie.Geocoder.Dto.Bing
 {
+    /// <summary>
+    /// Represents a point on the surface of the earth.
+    /// </summary>
     public class Point
     {
-        public List<double> BoundingBox { get; set; }
-
+        /// <summary>
+        /// The method that was used to compute the geocode point.
+        /// 
+        /// Can be one of the following values:
+        ///     Interpolation: The geocode point was matched to a point on a road using interpolation.
+        ///     InterpolationOffset: The geocode point was matched to a point on a road using interpolation with an additional offset to shift the point to the side of the street.
+        ///     Parcel: The geocode point was matched to the center of a parcel.
+        ///     Rooftop: The geocode point was matched to the rooftop of a building.
+        /// </summary>
         public string CalculationMethod { get; set; }
 
+        /// <summary>
+        /// The coordinate information of the point [Latitude,Longitude].
+        /// </summary>
         public List<double> Coordinates { get; set; }
 
+        /// <summary>
+        /// The best use for the geocode point. Can be Display or Route. 
+        /// Each geocode point is defined as a Route point, a Display point or both.
+        /// Use Route points if you are creating a route to the location.
+        /// 
+        /// Use Display points if you are showing the location on a map. 
+        /// 
+        /// For example, if the location is a park, a Route point may specify an entrance to the park where you can enter with a car, 
+        /// and a Display point may be a point that specifies the center of the park.
+        /// </summary>
         public List<string> UsageTypes { get; set; }
     }
 }
--- GeneGenie.Geocoder/Dto/Bing/Resource.cs
+++ GeneGenie.Geocoder/Dto/Bing/Resource.cs
@@ -5,23 +5,50 @@
 
 namespace GeneGenie.Geocoder.Dto.Bing
 {
+    /// <summary>
+    /// A location resource returned from the Bing location API.
+    /// </summary>
     public class Resource
     {
+        /// <summary>
+        /// The postal address for the location returned.
+        /// </summary>
         public Address Address { get; set; }
 
+        /// <summary>
+        /// Bounding box of the response. Structure [South Latitude, West Longitude, North Latitude, East Longitude].
+        /// </summary>
         [JsonProperty("bbox")]
         public List<double> BoundingBox { get; set; }
 
+        /// <summary>
+        /// The level of confidence that the geocoded location result is a match. Can be High, Medium, Low.
+        /// </summary>
         public string Confidence { get; set; }
 
+        /// <summary>
+        /// The classification of the geographic entity returned, such as Address.
+        /// </summary>
         public string EntityType { get; set; }
 
+        /// <summary>
+        /// A collection of geocoded points that differ in how they were calculated and their suggested use.
+        /// </summary>
         public List<Point> GeocodePoints { get; set; }
 
+        /// <summary>
+        /// One or more match code values that represent the geocoding level for each location in the response. Can be Good, Ambiguous, UpHierarchy.
+        /// </summary>
         public List<string> MatchCodes { get; set; }
 
+        /// <summary>
+        /// The name of the resource.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The latitude and longitude coordinates of the geocoded address.
+        /// </summary>
         public Point Point { get; set; }
     }
 }
